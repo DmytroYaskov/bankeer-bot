7a8aa2e009a0bb68eab54520a8a69e21529171e4
Dylan Sicarius <yaskov.1990@gmail.com>
2023-03-12 19:24:44 +0200

    Improved Docker container building logic

---
+++ .github/workflow/genNewImageUrl.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Set the container name to the name of the current GitHub repository
+container_name="${GITHUB_REPOSITORY##*/}"
+
+# Set the container version to the current UTC time and
+# the first 7 characters of the Git commit hash
+build_date=$(date -u +"%F_%R")
+commit_hash="${GITHUB_SHA::7}"
+container_version="$build_date+$commit_hash"
+
+# Output the Docker container tag using the registry name, container name,
+# and container version
+echo "registry.digitalocean.com/$REGISTRY/$container_name:$container_version"
