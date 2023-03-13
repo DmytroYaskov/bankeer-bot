7ddcc55336d78ad5d4265aeeda95a5da5309f714
Dylan Sicarius <yaskov.1990@gmail.com>
2023-03-13 09:41:47 +0200

    The image name was fixed, which caused an invalid reference format error.

--- .github/workflows/genNewImageUrl.sh
+++ .github/workflows/genNewImageUrl.sh
@@ -5,9 +5,9 @@ container_name="${GITHUB_REPOSITORY##*/}"
 
 # Set the container version to the current UTC time and
 # the first 7 characters of the Git commit hash
-build_date=$(date -u +"%F_%R")
+build_date=$(date -u +"%F_%H-%M")
 commit_hash="${GITHUB_SHA::7}"
-container_version="$build_date+$commit_hash"
+container_version="$build_date_$commit_hash"
 
 # Output the Docker container tag using the registry name, container name,
 # and container version
